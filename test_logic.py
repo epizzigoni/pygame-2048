d8444a9d75d7884156e015727211d4f2936aacc0
Edoardo Pizzigoni <edoardo.pizzigoni@gmail.com>
2020-11-10 13:05:41 +0100

    basic game logic done

--- test_logic.py
+++ test_logic.py
@@ -23,22 +23,60 @@ class Py2048Logic:
     @staticmethod
     def _get_nums(this):
         this_n = this[this != 0]
+        this_n_sum = []
+        skip = False
+        for j in range(len(this_n)):
+            if skip:
+                skip = False
+                continue
+            if j != len(this_n) - 1 and this_n[j] == this_n[j+1]:
+                new_n = this_n[j] * 2
+                skip = True
+            else:
+                new_n = this_n[j]
 
-        return this_n
+            this_n_sum.append(new_n)
+        return np.array(this_n_sum)
 
     def make_move(self, move):
         for i in range(N):
-            this = self.grid[i, :]
+            if move in 'lr':
+                this = self.grid[i, :]
+            else:
+                this = self.grid[:, i]
+
+            flipped = False
+            if move in 'rd':
+                flipped = True
+                this = this[::-1]
+
             this_n = self._get_nums(this)
 
             new_this = np.zeros_like(this)
             new_this[:len(this_n)] = this_n
-            self.grid[i, :] = new_this
+
+            if flipped:
+                new_this = new_this[::-1]
+
+            if move in 'lr':
+                self.grid[i, :] = new_this
+            else:
+                self.grid[:, i] = new_this
+
+    def play(self):
+        self.new_number(k=2)
+        while True:
+            print(self.grid)
+            cmd = input()
+            if cmd == 'q':
+                break
+            old_grid = self.grid.copy()
+            self.make_move(cmd)
+            if all((self.grid == old_grid).flatten()):
+                continue
+            self.new_number()
 
 
 if __name__ == '__main__':
     game = Py2048Logic()
-    game.new_number(k=2)
-    print(game)
-    game.make_move(move='l')
-    print(game)
+    game.play()
